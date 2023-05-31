33c541cd32a6361210bd720b3ed348d27238b18a
Jesus Borjas <jesusbor006@gmail.com>
2023-05-31 13:42:04 -0500

    Add component file structure and add navbar to page

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="main-app">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
